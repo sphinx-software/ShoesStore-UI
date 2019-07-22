d6393ae07d231ad1145d81253f4f4a0f91b5c51d
phichidung <dungpc@sphinx-software.com>
2019-07-22 18:38:55 +0700

    Add axios module

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import {
 }                         from "react-router-dom";
 import FormLogin          from "./components/Topbar/FormLogin";
 import FormRegister       from "./components/Topbar/FormRegister";
-import Home               from './views/Home';
+
 
 import HomePage          from './containers/HomePage';
 import ProductListPage   from './containers/ProductListPage';
@@ -17,9 +17,6 @@ export default class RouterProtect extends Component {
     render () {
         return (
             <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/login" component={FormLogin}/>
-                <Route exact path="/register" component={FormRegister}/>
                 <Route exact path="/" component={HomePage}/>
                 <Route exact path="/login" component={FormLogin}/>
                 <Route exact path="/register" component={FormRegister}/>
