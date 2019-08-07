22d15ef8107e4d22d231ce6582d9d57ac51a3d12
vnduy01 <duynt@sphinx-software.com>
2019-08-07 13:47:40 +0700

    add ThumbProduct component

--- src/components/Illustrationv5/Illustrationv5.js
+++ src/components/Illustrationv5/Illustrationv5.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import ThumbProduct from '../GridItem/ThumbProduct';
 import Nav from "reactstrap/es/Nav";
-import NavLink from "reactstrap/es/NavLink";
 
 export default class Illustrationv5 extends Component {
     render() {
+
         return (
             <Nav>
                 <div className="row illustration-v4 margin-bottom-40">
@@ -11,118 +12,25 @@ export default class Illustrationv5 extends Component {
                         <div className="heading heading-v1 margin-bottom-20">
                             <h2>Top Rated</h2>
                         </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/08.jpg" alt={'text'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price line-through">$75.00</li>
-                                <li className="thumb-product-price">$65.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/09.jpg" alt={'thump-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price">$75.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/03.jpg" alt={'thumb-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price">$75.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
+                        <ThumbProduct/>
+                        <ThumbProduct/>
+                        <ThumbProduct/>
                     </div>
                     <div className="col-md-4">
                         <div className="heading heading-v1 margin-bottom-20">
                             <h2>Best Sellers</h2>
                         </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/02.jpg" alt={'thump-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price">$75.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/10.jpg" alt={'thumb-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price">$75.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/06.jpg" alt={'thumb-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price line-through">$75.00</li>
-                                <li className="thumb-product-price">$65.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
+                        <ThumbProduct/>
+                        <ThumbProduct/>
+                        <ThumbProduct/>
                     </div>
                     <div className="col-md-4 padding">
                         <div className="heading heading-v1 margin-bottom-20">
                             <h2>Sale Items</h2>
                         </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/07.jpg" alt={'thumb-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price line-through">$75.00</li>
-                                <li className="thumb-product-price">$65.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/04.jpg" alt={'thumb-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price">$75.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="thumb-product">
-                            <img className="thumb-product-img" src="assets/img/thumb/05.jpg" alt={'thumb-product'} />
-                            <div className="thumb-product-in">
-                                <h4><NavLink href="shop-ui-inner.html">Yuketen</NavLink> – <NavLink href="shop-ui-inner.html">Derby Shoe</NavLink></h4>
-                                <span className="thumb-product-type">Footwear - Oxfords</span>
-                            </div>
-                            <ul className="list-inline overflow-h">
-                                <li className="thumb-product-price">$75.00</li>
-                                <li className="thumb-product-purchase"><NavLink href="#"><i className="fa fa-shopping-cart" /></NavLink> | <NavLink href="#"><i className="fa fa-heart" /></NavLink></li>
-                            </ul>
-                        </div>
+                        <ThumbProduct/>
+                        <ThumbProduct/>
+                        <ThumbProduct/>
                     </div>
                 </div>
             </Nav>
