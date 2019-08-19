aef9e257d15f25431ae943776a0240476e0b986e
LongHai97 <haitran6397@gmail.com>
2019-08-19 11:09:59 +0700

    create gradient banner products page

--- src/components/Banner/HeadBanner.js
+++ src/components/Banner/HeadBanner.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+import "../../ui/banner/headbanner.css";
+
 export default class HeadBanner extends Component {
     render() {
         return(
-            <div className="breadcrumbs-v4">
+            <div className="headbanner">
                 <div className="container">
                     <span className="page-name">ShoesStore</span>
                     <h1>Choice <span className="shop-green">for</span> everyone</h1>
